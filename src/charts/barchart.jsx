60d0180ad861d12e3e54477402c2e77b9740da9f
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-20 17:09:12 +0530

    react-router

--- src/charts/barchart.jsx
+++ src/charts/barchart.jsx
@@ -27,8 +27,20 @@ const BarChart = () => {
       {
         label: 'Weather Stat',
         data: [12, 19, 3, 5, 16],
-        backgroundColor: 'rgba(75, 192, 192, 0.6)',
-        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+        ],
+        borderColor: [
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
         borderWidth: 1,
       },
     ],
