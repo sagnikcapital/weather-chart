15f1a30e4cb19962f6046d9153c36fbab2eb0eec
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-20 12:20:29 +0530

    react-router

---
+++ src/charts/barchart.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+const BarChart = () => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        label: 'Sales',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+}
+
+export default BarChart;
---
+++ src/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './home.css';
+
+function Home(){
+  return(
+    <p>Home</p>
+  );
+}
+export default Home;
