4ed92ebde25196088e25e54eb43945c51ab690d1
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-20 12:44:26 +0530

    react-router

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import PieChart from '../charts/piechart';
+import BarChart from '../charts/barchart';
 import './home.css';
 
 function Home(){
   return(
-    <p>Home</p>
+    <div className="App container">
+      <h1 className="text-center my-4">My React Charts</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card mb-4 shadow-sm">
+            <div className="card-header">
+              <h5 className="card-title">Weather Chart</h5>
+            </div>
+            <div className="card-body">
+              {/* <BarChart /> */}
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card mb-4 shadow-sm">
+            <div className="card-header">
+              <h5 className="card-title">Votes Pie Chart</h5>
+            </div>
+            <div className="card-body">
+              {/* <PieChart /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 export default Home;
