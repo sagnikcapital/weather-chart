34517d713373940fdc2fd1c1a6bac5fc17863a8e
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-29 15:07:29 +0530

    read-md

--- src/chatbot/bot.jsx
+++ src/chatbot/bot.jsx
@@ -1,8 +1,10 @@
 import  { useState, useEffect } from 'react';
+import loadTawkTo from './botService';
 
-
-const Bot = () => {
-
+const Bot = ({ propertyId }) => {
+  return (
+    loadTawkTo(propertyId)
+  );
 }
 
 export default Bot;
