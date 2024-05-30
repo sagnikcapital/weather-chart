18aaf996dac539b15b5cd31fbaf2b576ea83f965
sagnikcapital <sagnikd@capitalnumbers.com>
2024-05-30 16:46:41 +0530

    about

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -6,12 +6,13 @@ const About = () => {
   const [aboutValue, setAboutValue] = useState('Hi');
 
   useEffect(()=>{
-
+    /**On mounted Dom*/
+    setAboutValue('I am sagnik');
   },[]);
 
   const paraRef = useRef();
 
-  setAboutValue('This is a new line');
+  // setAboutValue('This is a new line');
 
   return (
     <p className="text-align-center paragraph">{ aboutValue }</p>
