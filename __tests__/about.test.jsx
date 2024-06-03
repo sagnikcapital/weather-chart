84f38e69a9db4e96a178ba2ab533f9587db8dcad
sagnikcapital <sagnikd@capitalnumbers.com>
2024-06-03 14:24:12 +0530

    settings-json

--- __tests__/about.test.jsx
+++ __tests__/about.test.jsx
@@ -4,3 +4,10 @@ import { setupServer } from 'msw/node';
 import { render, fireEvent, screen } from '@testing-library/react';
 import About from '../src/pages/about/about';
 import '@testing-library/jest-dom';
+
+// Set up the server
+const server = setupServer(
+  rest.get('/api/react/test/about', (req, res, ctx) => {
+    return res(ctx.json({ greeting: 'About Page testing' }));
+  }),
+);
